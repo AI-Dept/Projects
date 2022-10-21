d8abcb4553dfccb0433dba4b2e13039888cb8370
Aditya Komaravolu <64011471+Aditya-Komaravolu@users.noreply.github.com>
2022-10-22 00:18:34 +0530

    added features to test.py

--- test_project/test.py
+++ test_project/test.py
@@ -2,4 +2,4 @@ import numpy as np
 print("hello github!")
 print("this is a test file to check whether lint is working or not")
 
-print("output:", np.arange(0, 90, 5))
+print("output:", np.arange(0, 80, 5))
